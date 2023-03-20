03f6878b61894f5ba1db5c940e44bd3e39252c3a
juanedcabrera <juanedcabrera@gmail.com>
2023-03-20 16:16:07 -0600

    Going back to original

--- scripts.js
+++ scripts.js
@@ -3,32 +3,61 @@
 // Rendering is too slow
 // Images need to be flipped based on direction
 
-//REFACTOR
 
-
-const state = {
 // DOM Selectors
-selector: { 
-  startBtn: document.querySelector("#startBtn"),
-  canvas: document.querySelector("#canvas"),
-  ctx: canvas.getContext('2d', {willReadFrequently: true}),
-  timer: document.querySelector("#timer"),
-  stage: {
+const startBtn = document.querySelector("#startBtn");
+const canvas = document.querySelector("#canvas");
+const ctx = canvas.getContext('2d', {willReadFrequently: true});
+const timer = document.querySelector("#timer");
+const stage = {
   width: 800,
   height: 250,
 }
-}
-}
 
+// PRE-LOAD maybe we can pre-render
 // IMAGES
-function buildImageUrls (number) {
-  const peepImageArray = []
+// Array to hold peep images
+let peepImageArray = []
+
+// testing for one image to display on canvas
+// // Create image through javascript vs html
+// var img = new Image();
+// img.src = "assets/wilma.png";
+// // makes sure image loads before function is run
+// img.onload = function(){
+
+//   // draw the image onto the canvas
+//   ctx.drawImage(img, 0, 0, 75, 100);
+  
+//   // get the image data from the canvas
+//   var imageData = ctx.getImageData(0, 0, 75, 100);
+  
+//   // add the image data to the peepImageArray
+//   peepImageArray.push(imageData);
+// }
+// console.log (peepImageArray) // worked
+
+// IMAGE PEEP LOOP
+
 
 // for loop to create multiple images. Right now it is linked to 9 total images.
-for (let i = 1; i < number; i++) {
-  peepImageArray.push(`assets/peep${i}.png`);
-}}
+for (let i = 1; i < 50; i++) {
+  let img = new Image();
+  img.src = `assets/peep${i}.png`;
+  img.data = '0, 0, 75, 100'
+  peepImageArray.push({
+    src: img.src,
+    data: img.data}
+    )
+  }
+console.log(peepImageArray) //worked
 
+
+// CREATE PEEPS
+// peep array
+let peepArray = [];
+
+// constructor to make peeps and it adds the peep to the peep array
 class Peep {
   // this.image
   constructor(x, y, width, height, color, speed, direction, src, data) {
@@ -54,22 +83,24 @@ class Peep {
   }
 }
 
-function createPeeps () {
-  let peepArray = []
+const waldo = new Peep(0, 0, 0, 0, "red", 5, "right", 'http://127.0.0.1:5500/assets/waldo3.jpeg', ("0, 0, 75, 100"));
 
-  for (let i =0; i < 45; i++) {
-    new Peep(0, 0, 0, 0, "#F88379", 5, "left", peepImageArray[i].src, peepImageArray[i].data) };
+for (let i =0; i < 5; i++) {
+  new Peep(0, 0, 0, 0, "#F88379", 5, "left", peepImageArray[i].src, peepImageArray[i].data) };
 
-  const waldo = new Peep(0, 0, 0, 0, "red", 5, "right", 'http://127.0.0.1:5500/assets/waldo3.jpeg', ("0, 0, 75, 100"));
-}
 
-function rondomizePeep(peep) {
+// put images in array peepImageArray
+
+console.log(peepArray); // worked
+
+// RANDOMIZE PEEP
+function randomizePeep(peep) {
   //use code to pick random image from peepImageArray and set it
   peep.width = 150;
   peep.height = 200
   peep.x = Math.random() < 0.5 ? 0 - peep.width: 750;
   peep.y = stage.height + 205 - Math.floor(Math.random() * 250);
-  peep.speed = Math.floor(Math.random() * (10 - 5 + 1) + 5);
+  peep.speed = Math.floor(Math.random() * (30 - 5 + 1) + 5);
   //this stops the peek-a-boo
   peep.direction = peep.x > 100 ? "left" : "right";
   peep.src = peep.src
@@ -77,22 +108,47 @@ function rondomizePeep(peep) {
   console.log(peep)
 }
 
-function startGame () {{
+
+// GAME MECHANICS
+
 let gameLoopInterval;
+let timeSecond = 60;
 let gameOver = true;
 
-    gameLoopInterval = setInterval(gameLoop, 60);
-    startBtn.disabled = true;
-    timeInterval;
-    gameOver = false;
-  }
-  
-  startBtn.addEventListener("click", startGame);
+// TIMER
+
+timer.innerText = `:${timeSecond}`;
+
+let timeInterval;
+
+startBtn.addEventListener("click", function() {
+  timeInterval = setInterval(() => {
+    timeSecond--;
+    timer.innerText = `:${timeSecond}`;
+    if (timeSecond <= 0) {
+      clearInterval(timeInterval);
+      gameEnd();
+      timeSecond = 60;
+      timer.innerText = `:${timeSecond}`;
+      ctx.font = "30px Arial";
+      ctx.fillStyle = "black";
+      ctx.textAlign = "center";
+      ctx.fillText("Waldo Escaped", canvas.width/2, canvas.height/2);
+    }
+  }, 1000);
+});
+
+// START GAME
+function startGame() {
+  gameLoopInterval = setInterval(gameLoop, 60);
+  startBtn.disabled = true;
+  timeInterval;
+  gameOver = false;
 }
 
-function update()
+startBtn.addEventListener("click", startGame);
 
-function render()
+// GAME LOOP
 
 function gameLoop() {
   if (!gameOver) {
@@ -117,32 +173,9 @@ function gameLoop() {
   }
 }
 
-function timer () {
-  
-let timeSecond = 60;
-let timeInterval;
+// WALDO
 
-timer.innerText = `:${timeSecond}`;
-
-startBtn.addEventListener("click", function() {
-  timeInterval = setInterval(() => {
-    timeSecond--;
-    timer.innerText = `:${timeSecond}`;
-    if (timeSecond <= 0) {
-      clearInterval(timeInterval);
-      gameEnd();
-      timeSecond = 60;
-      timer.innerText = `:${timeSecond}`;
-      ctx.font = "30px Arial";
-      ctx.fillStyle = "black";
-      ctx.textAlign = "center";
-      ctx.fillText("Waldo Escaped", canvas.width/2, canvas.height/2);
-    }
-  }, 1000);
-});
-}
-
-function waldo () {
+//When you click on waldo initiates gameEnd function
 canvas.addEventListener("click", (e) => {
   if (
     e.offsetX >= waldo.x &&
@@ -159,8 +192,9 @@ canvas.addEventListener("click", (e) => {
     ctx.textAlign = "center";
     ctx.fillText("Caught Waldo", canvas.width/2, canvas.height/2);
   }
-})}
+});
 
+// END GAME
 function gameEnd() {
   clearInterval(timeInterval);
   clearInterval(gameLoopInterval);
@@ -171,177 +205,3 @@ function gameEnd() {
   peepArray.forEach((peep) => {
     randomizePeep(peep)
 })}
-
-
-
-// // IMAGES
-// // Array to hold peep images
-// let peepImageArray = []
-
-// // for loop to create multiple images. Right now it is linked to 9 total images.
-// for (let i = 1; i < 50; i++) {
-//   let img = new Image();
-//   img.src = `assets/peep${i}.png`;
-//   img.data = '0, 0, 75, 100'
-//   peepImageArray.push({
-//     src: img.src,
-//     data: img.data}
-//     )
-//   }
-// console.log(peepImageArray) //worked
-
-
-// CREATE PEEPS
-// peep array
-// let peepArray = [];
-
-// constructor to make peeps and it adds the peep to the peep array
-// class Peep {
-//   // this.image
-//   constructor(x, y, width, height, color, speed, direction, src, data) {
-//     this.x = x;
-//     this.y = y;
-//     this.width = width;
-//     this.height = height;
-//     this.color = color;
-//     this.speed = speed;
-//     this.direction = direction;
-//     this.src = src
-//     this.data = data
-//     peepArray.push(this);
-//   }
-//   // add image to render - this is what is making my image
-//   render() {
-//     const img = new Image();
-//     img.src = this.src
-//     img.onload = () => {
-//       ctx.drawImage(img, this.x, this.y, this.width, this.height);
-//       this.data = ctx.getImageData(0, 0, img.width, img.height);
-//     }
-//   }
-// }
-
-// const waldo = new Peep(0, 0, 0, 0, "red", 5, "right", 'http://127.0.0.1:5500/assets/waldo3.jpeg', ("0, 0, 75, 100"));
-
-// for (let i =0; i < 45; i++) {
-//   new Peep(0, 0, 0, 0, "#F88379", 5, "left", peepImageArray[i].src, peepImageArray[i].data) };
-
-
-// put images in array peepImageArray
-
-// console.log(peepArray); // worked
-
-// // RANDOMIZE PEEP
-// function randomizePeep(peep) {
-//   //use code to pick random image from peepImageArray and set it
-//   peep.width = 150;
-//   peep.height = 200
-//   peep.x = Math.random() < 0.5 ? 0 - peep.width: 750;
-//   peep.y = stage.height + 205 - Math.floor(Math.random() * 250);
-//   peep.speed = Math.floor(Math.random() * (10 - 5 + 1) + 5);
-//   //this stops the peek-a-boo
-//   peep.direction = peep.x > 100 ? "left" : "right";
-//   peep.src = peep.src
-//   peep.data = peep.data;
-//   console.log(peep)
-// }
-
-
-// GAME MECHANICS
-
-// let gameLoopInterval;
-// // let timeSecond = 60;
-// let gameOver = true;
-
-// TIMER
-
-// timer.innerText = `:${timeSecond}`;
-
-// let timeInterval;
-
-// startBtn.addEventListener("click", function() {
-//   timeInterval = setInterval(() => {
-//     timeSecond--;
-//     timer.innerText = `:${timeSecond}`;
-//     if (timeSecond <= 0) {
-//       clearInterval(timeInterval);
-//       gameEnd();
-//       timeSecond = 60;
-//       timer.innerText = `:${timeSecond}`;
-//       ctx.font = "30px Arial";
-//       ctx.fillStyle = "black";
-//       ctx.textAlign = "center";
-//       ctx.fillText("Waldo Escaped", canvas.width/2, canvas.height/2);
-//     }
-//   }, 1000);
-// });
-
-// // START GAME
-// function startGame() {
-//   gameLoopInterval = setInterval(gameLoop, 60);
-//   startBtn.disabled = true;
-//   timeInterval;
-//   gameOver = false;
-// }
-
-// startBtn.addEventListener("click", startGame);
-
-// GAME LOOP
-
-// function gameLoop() {
-//   if (!gameOver) {
-//     // this is for the movement of the boxes
-//     ctx.clearRect(0, 0, canvas.width, canvas.height);
-//     // making peeps appear
-//     console.log(peepArray)
-//     peepArray.forEach((peep) => {
-//       // moving the peep left or right
-//       if (peep.direction === "left") {
-//         peep.x -= peep.speed;
-//       } else {
-//         peep.x += peep.speed;
-//       }
-//       // staying in canvas
-//       if (peep.x < -peep.width || peep.x > canvas.width) {
-//         randomizePeep(peep);
-//       }
-//       // draw peep
-//       peep.render ()
-//     });
-//   }
-// }
-
-// WALDO
-
-//When you click on waldo initiates gameEnd function
-// canvas.addEventListener("click", (e) => {
-//   if (
-//     e.offsetX >= waldo.x &&
-//     e.offsetX <= waldo.x + waldo.width &&
-//     e.offsetY >= waldo.y &&
-//     e.offsetY <= waldo.y + waldo.height
-//   ) {
-//     gameEnd();
-//     clearInterval(timeInterval);
-//     timeSecond = 60;
-//     timer.innerText = `:${timeSecond}`;
-//     ctx.font = "30px Arial";
-//     ctx.fillStyle = "black";
-//     ctx.textAlign = "center";
-//     ctx.fillText("Caught Waldo", canvas.width/2, canvas.height/2);
-//   }
-// });
-
-// END GAME
-// function gameEnd() {
-//   clearInterval(timeInterval);
-//   clearInterval(gameLoopInterval);
-//   timeSecond = 60;
-//   ctx.clearRect(0, 0, canvas.width, canvas.height);
-//   gameOver = true;
-//   startBtn.disabled = false;
-//   peepArray.forEach((peep) => {
-//     randomizePeep(peep)
-// })}
-
-
